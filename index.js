0e19852c3a09f6feccf4c09e6d647645ec33f92b
Fysiks <fysiks1@gmail.com>
2021-06-18 23:23:11 -0500

    log GITHUB_WORKSPACE environment variable

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`GITHUB_WORKSPACE is ${process.env.GITHUB_WORKSPACE}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
